2142c253e0d6c34c58270036035c8b16f43a1cd0
tdagai <trew3535@gmail.com>
2026-09-01 13:33:35 -0700

    items are ordered correctly for mobile size. going to fix styling next to make sure it looks good.

--- src/app/components/HomepageHero/HomepageHero.tsx
+++ src/app/components/HomepageHero/HomepageHero.tsx
@@ -6,8 +6,8 @@ import HeroImage from "@assets/homepage/hero.jpg";
 
 const HomepageHero = () => {
   return (
-    <section className="mt-24 grid w-full grid-cols-12 gap-8">
-      <div className="row-1 col-start-2 col-end-7 flex flex-col justify-evenly">
+    <section className="grid w-full gap-4 sm:grid-cols-4 md:mt-24 md:grid-cols-12">
+      <div className="flex flex-col justify-evenly gap-8 sm:col-start-1 sm:col-end-5 sm:row-start-2 md:col-start-2 md:col-end-7 md:row-start-1">
         <h1 className="text-primary font-young">
           A Versatile Artist For All Your Makeup Needs
         </h1>
@@ -16,11 +16,11 @@ const HomepageHero = () => {
         </h3>
         <StyledButton>{"Reach Out!"}</StyledButton>
       </div>
-      <div className="col-start-8 col-end-12">
+      <div className="row-start-1 sm:col-start-1 sm:col-end-5 sm:w-full md:col-start-8 md:col-end-12 md:w-auto">
         <Image
           src={HeroImage}
           alt="a picture of Geffen on a lit green background"
-          className="shadow-big rounded-2xl"
+          className="shadow-big rounded-b-2xl md:rounded-2xl"
         />
       </div>
     </section>
